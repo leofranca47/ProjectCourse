4d011c7c7fd5b65199cf6c70ffd645346ef36fcf
leofranca47 <leoprofissional@live.com>
2021-05-12 07:00:42 -0300

    adicionado seo

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,27 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home()
     {
-        return view('front.home');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Home - escola',
+            'Essa escola vai te levar para o proximo nivel',
+            url('/'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.home', [ 'head' => $head ]);
     }
 
     public function course()
     {
-        return view('front.course');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Course - escola',
+            'Essa escola vai te levar para o proximo nivel',
+            url('/'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.course', [ 'head'=>$head ]);
     }
 
     public function blog()
     {
-        return view('front.blog');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Blog - escola',
+            'Essa escola vai te levar para o proximo nivel',
+            url('/'),
+            asset('images/img_bg_1.jpg')
+        );
+
+        return view('front.blog', [ 'head'=>$head ]);
     }
 
     public function contact()
     {
-        return view('front.contact');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Contact - escola',
+            'Essa escola vai te levar para o proximo nivel',
+            url('/'),
+            asset('images/img_bg_1.jpg')
+        );
+        return view('front.contact', [ 'head'=>$head ]);
     }
 }
